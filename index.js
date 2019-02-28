8c87709fa4d5fa1382ac42f1780cad7ccb05a61e
Ivan Rubinson <soryy708@gmail.com>
2019-02-28 12:13:44 +0200

    Auto-update round information every minute

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 $(function () { //shorthand document.ready function
+    updateData();
+    setInterval(updateData, 1000 * 60); // update every minute
 
-    $.get("https://info.baystation12.net/status", function (data) {
-        $("#player").html(data.players);
-        $("#mode").html(data.mode);
-        $("#admin").html(decodeURIComponent(data.roundduration));
-    });
-
-
+    function updateData() {
+        $.get("https://info.baystation12.net/status", function (data) {
+            $("#player").html(data.players);
+            $("#mode").html(data.mode);
+            $("#admin").html(decodeURIComponent(data.roundduration));
+        });
+    }
 });
