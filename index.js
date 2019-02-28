56d01e2e6d042dfee6f7d315b920a01ad09ecf62
Ivan Rubinson <soryy708@gmail.com>
2019-02-28 21:16:34 +0200

    Make notifications show when round ends instead of starts.

--- index.js
+++ index.js
@@ -1,56 +1,52 @@
 $(function () { //shorthand document.ready function
-    var lastTime = 0;
-
     if (window.Notification) {
         window.Notification.requestPermission();
     }
+
+    var justNotified = false;
 
     updateData();
     setInterval(updateData, 1000 * 60); // update every minute
 
     function updateData() {
         $.get("https://info.baystation12.net/status", function (data) {
-            if (data && Object.keys(data).length > 1) {// round is on-going
-                var roundDuration = decodeURIComponent(data.roundduration);
-                $("#player").html(data.players);
-                $("#mode").html(data.mode);
-                $("#admin").html(roundDuration);
+            if (!data) data = {};
+            var roundDuration = data.roundduration ? decodeURIComponent(data.roundduration) : null;
+            $("#player").html(data.players || '--');
+            $("#mode").html(data.mode || '--');
+            $("#admin").html(roundDuration || '--:--');
+            
+            if (roundDuration && roundDuration !== '00:00') { // Round is on-going
+                justNotified = false;
+            }
 
-                var currentTime = parseTime(roundDuration);
-                if(currentTime !== null) { // Succeeded parsing
-                    if (currentTime < lastTime) { // Restart detected
-                        if (window.Notification && window.Notification.permission == 'granted') {
-                            var notification = new window.Notification('New Baystation12 round started!', {
-                                body: 'Game mode: ' + data.mode + '.\nClick here to go directly to the round.',
-                                icon: 'https://baystation12.net/forums/logo.png',
-                                image: 'https://baystation12.net/forums/logo.png'
-                            });
-                            notification.onclick = function() {
-                                var newWindow = window.open('byond://play.baystation12.net:8000');
-                                setTimeout(function() {
-                                    if(newWindow) {
-                                        newWindow.close();
-                                    }
-                                }, 500);
-                            }
+            if (shouldNotify()) {
+                var notification = new window.Notification('Baystation12 round ended!', {
+                    body: 'Click here to go join the lobby.',
+                    icon: 'https://baystation12.net/forums/logo.png',
+                    image: 'https://baystation12.net/forums/logo.png'
+                });
+                justNotified = true;
+                notification.onclick = function() {
+                    var newWindow = window.open('byond://play.baystation12.net:8000');
+                    setTimeout(function() {
+                        if(newWindow) {
+                            newWindow.close();
                         }
-                    }
-                } else {
-                    console.log('Could\'nt parse time string');
+                    }, 500);
                 }
             }
-        });
-    }
 
-    function parseTime(timeStr) {
-        if (!timeStr) return null;
-        if (timeStr.indexOf(':') == -1) return null;
-        const parts = timeStr.split(':');
-        if (parts.length != 2) return null;
-        const hours = parts[0];
-        const minutes = parts[1];
-        const hoursNum = Number(hours);
-        const minutesNum = Number(minutes);
-        return (hoursNum * 100) + minutesNum;
+            function shouldNotify() {
+                return (
+                    !justNotified && (
+                        !data.players || !data.mode || !data.roundduration || // Server is down
+                        (data.roundduration && decodeURIComponent(data.roundduration) == '00:00') // In lobby or just started
+                    ) &&
+                    window.Notification && // Notifications are supported
+                    window.Notification.permission == 'granted' // Permission was granted
+                );
+            }
+        });
     }
 });
